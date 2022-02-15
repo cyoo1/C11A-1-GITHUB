c9ec2be50bb1a1c2c72046c9e64aa04edd6d874c
cyoo1 <yoo.chrisx15@gmail.com>
2022-02-15 12:59:20 -0700

    increased face size and eye size one more time

--- src/Cat.java
+++ src/Cat.java
@@ -5,14 +5,14 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 200;
+	private static final int HEAD_DIMENSION = 300;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 50;
-	private static final int EYE_WIDTH = 30;
+	private static final int EYE_HEIGHT = 90;
+	private static final int EYE_WIDTH = 50;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
@@ -32,7 +32,7 @@ public class Cat {
 		g2.setColor(LIGHT_BROWN);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.green);
+		g2.setColor(Color.yellow);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
